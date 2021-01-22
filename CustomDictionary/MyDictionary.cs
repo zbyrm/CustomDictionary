b0a2dcd8aa61a32abecb7b16a34fd8348c3d0e79
zbyrm <ziyax@hotmail.com>
2021-01-22 09:19:05 +0300

    Add project files.

---
+++ CustomDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDictionary
+{
+    
+    class MyDictionary<TKey, TValue>
+    {
+        List<TKey> _keys;
+        List<TValue> _values;
+
+        public MyDictionary()
+        {
+            _keys = new List<TKey>();
+            _values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (_keys == null)
+                throw new  ArgumentNullException("key is null (key deger atanmamis)");
+
+            if (_keys.Contains(key) )
+            {
+                //aynı keye sahip eleman var
+                //An element with the same key already exists
+                throw new ArgumentException("An element with the same key already exists (Anahtar degeri mevcut)");
+            }
+
+            _keys.Add(key);
+            _values.Add(value);
+        }
+
+        public TValue this[TKey key] 
+        {   
+            get
+            {
+                int index = _keys.IndexOf(key);
+
+                if (key==null)
+                    throw new ArgumentNullException(" key is null (key deger atanmamis)") ;
+
+                if (index == -1)
+                    throw new KeyNotFoundException("key does not exist in the collection (Anahtar deger mevcut degil)");
+
+                TValue value = _values[index];
+
+                return value;
+            }
+
+        }
+    }
+}
---
+++ CustomDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            students.Add(340, "Guven Gamsiz");
+            
+
+            Console.WriteLine(students[340]);
+
+            MyDictionary<int, string> students2 = new MyDictionary<int, string>();
+            students2.Add(83, "Mesut Bahtiyaroğlu");
+            // students2.Add(83, "Saim Gelgör"); //ArgumentException  An element with the same key already exists
+
+            Console.WriteLine(students2[83]);
+
+        }
+    }
+}
